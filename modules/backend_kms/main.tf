d4fde7b39f120acca3375de542ebcac854256538
aramis.mendozaurbano <aramis.mendozaurbano@plexus.es>
2022-01-09 18:27:06 +0100

    Initial commit. Add Backends modules

--- modules/backend_kms/main.tf
+++ modules/backend_kms/main.tf
@@ -1,58 +1,62 @@
 resource "aws_kms_key" "kms_key" {
- description             = "This key is used to encrypt bucket objects"
- deletion_window_in_days = var.deletion_windows_day
- enable_key_rotation     = var.enable_key_rotation
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = var.deletion_windows_day
+  enable_key_rotation     = var.enable_key_rotation
 }
 
 resource "aws_kms_alias" "key_alias" {
- name          = var.key_alias
- target_key_id = aws_kms_key.kms_key.id
+  name          = var.key_alias
+  target_key_id = aws_kms_key.kms_key.id
 }
 
 resource "aws_s3_bucket" "terraform_state" {
- bucket = var.s3_bucket_name
- acl    = "private"
+  bucket = var.s3_bucket_name
+  acl    = "private"
 
- versioning {
-   enabled = true
- }
+  versioning {
+    enabled = true
+  }
 
- lifecycle {
-   prevent_destroy = true
- }
+  lifecycle {
+    prevent_destroy = true
+  }
 
- server_side_encryption_configuration {
-   rule {
-     apply_server_side_encryption_by_default {
-       kms_master_key_id = aws_kms_key.kms_key.id
-       sse_algorithm     = "aws:kms"
-     }
-   }
- }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.kms_key.id
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "block" {
- bucket = aws_s3_bucket.terraform_state.id
+  bucket = aws_s3_bucket.terraform_state.id
 
- block_public_acls       = true
- block_public_policy     = true
- ignore_public_acls      = true
- restrict_public_buckets = true
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_dynamodb_table" "terraform_state" {
- name           = var.dynamodb_table_name
- read_capacity  = var.read_capacity
- write_capacity = var.write_capacity
- hash_key       = "LockID"
+  name           = var.dynamodb_table_name
+  read_capacity  = var.read_capacity
+  write_capacity = var.write_capacity
+  hash_key       = "LockID"
 
- server_side_encryption {
-   enabled = var.dynamodb_encryption
-   kms_key_arn = aws_kms_key.kms_key.arn
- }
+  server_side_encryption {
+    enabled     = var.dynamodb_encryption
+    kms_key_arn = aws_kms_key.kms_key.arn
+  }
 
- attribute {
-   name = "LockID"
-   type = "S"
- }
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  depends_on = [
+    aws_kms_key.kms_key,
+  ]
 }
--- modules/backend_kms/variables.tf
+++ modules/backend_kms/variables.tf
@@ -1,46 +1,46 @@
 
 variable "enable_key_rotation" {
-    type = bool
-    description = "(optional) Enable (true) / Disable(false), the rotation of the key. Default = 'Enable' (true)"
-    #default = true
+  type        = bool
+  description = "(optional) Enable (true) / Disable(false), the rotation of the key. Default = 'Enable' (true)"
+  #default = true
 }
 
 variable "deletion_windows_day" {
-    type = number
-    description = "(optional) Deletion windows for rotate the KMS Keys"
-    default = 10
+  type        = number
+  description = "(optional) Deletion windows for rotate the KMS Keys"
+  default     = 10
 }
 
 variable "key_alias" {
-    type = string
-    description = "(required) Alias for the KMS Key"
+  type        = string
+  description = "(required) Alias for the KMS Key"
 }
 
 variable "s3_bucket_name" {
-  type = string
+  type        = string
   description = "(required) Unique identifier bucket name"
 }
 
 variable "dynamodb_table_name" {
-  type = string
+  type        = string
   description = "(required) DynamoDB Table name for lock the tfstate"
 }
 
 variable "read_capacity" {
-  type = number
+  type        = number
   description = "(optional) max capacity of simultaneously read requests"
-  default = 1
+  default     = 1
 }
 
 variable "write_capacity" {
-  type = number
+  type        = number
   description = "(optional) max capacity of simultaneosly write requests"
-  default = 1
+  default     = 1
 }
 
 variable "dynamodb_encryption" {
-  type = bool
+  type        = bool
   description = "(optional) describe your variable"
-  default = true
+  default     = true
 }
 
