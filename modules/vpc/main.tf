acc1e584dfcbf76fae9287d0ead02b1ebae765f2
aramis.mendozaurbano <aramis.mendozaurbano@plexus.es>
2022-01-10 12:20:08 +0100

    Modify vpc module

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -17,7 +17,7 @@ resource "aws_vpc" "this" {
 }
 
 resource "aws_subnet" "public" {
-  for_each = var.cidr_block
+  for_each = var.public_cidr_block
   vpc_id                          = aws_vpc.this.id
   cidr_block                      = each.value
   availability_zone_id            = data.aws_availability_zones.available[each.key]
