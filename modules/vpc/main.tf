c61dffffe9d2fab21553f5722f4212047609a9f0
aramis.mendozaurbano <aramis.mendozaurbano@plexus.es>
2022-01-10 15:12:43 +0100

    Add Tags in module VPC

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -27,5 +27,6 @@ resource "aws_subnet" "public" {
 
   tags = {
     "Name" = "${local.subnet_prefix}-${var.subnet_names[each.key]}-${local.az_names[each.key]}",
+    "Tags" = var.project_tags
   }
 }
